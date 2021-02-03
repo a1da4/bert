aaf63267c8e0859ad11dd4d886ce9e478513d728
Aida <45454055+a1da4@users.noreply.github.com>
2021-02-03 14:57:07 +0900

    Update build_vocab.sh
    fix path

--- build_vocab.sh
+++ build_vocab.sh
@@ -1,5 +1,5 @@
 python3 build_vocab.py \
-    --input_file ~/work/lsm/data/COHA/1900s-vs-1990s.txt \
-    --output_file ~/work/lsm/1900vs1990/word/bert/train-from-scratch/vocab.txt \
+    --input_file path/to/data/data.txt \
+    --output_file path/to/vocab/vocab.txt \
     --subword_type bpe \
     --vocab_size 32000                        
